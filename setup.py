05864fba5f74213932f81ed7089760e26cdbb9cb
Gal Ben Haim <gal.benhaim@solaredge.com>
2019-10-11 03:16:18 +0300

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='TemplateCreator',
     packages=['TemplateCreator'],
-    version='0.8',
+    version='0.9',
     license='MIT',
     description='This project is intended unify boilerplate code between team members and make the setup process for starting a new component shorted and less tedious.',
     author='Gal Ben Haim',
